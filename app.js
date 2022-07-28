6a58acc6fb70441271046c13f69ce5fa378bb276
ycoodr <ycoodr@gmail.com>
2022-07-28 17:29:28 +0300

    mongoose

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/vidjot-dev')
+.then(() => console.log('MongoDb connected...'))
+.catch(err => console.log(err));
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
