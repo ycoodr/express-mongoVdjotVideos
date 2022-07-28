e969ac2bf11e28111e2f9ccad653d4e5fa18601e
ycoodr <ycoodr@gmail.com>
2022-07-28 16:42:51 +0300

    handlebars

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const port = 5000;
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req, res) => {
+    const title = 'Welcome1';
+    res.render('index', { title: title });
+    // res.send('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 
 app.listen(port, () => {
