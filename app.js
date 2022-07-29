aae2d8fac297e5df46731c92b9b2ebec4d37f849
ycoodr <ycoodr@gmail.com>
2022-07-29 12:32:06 +0300

    edit idea

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 const port = 5000;
 
@@ -18,6 +19,7 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
@@ -30,8 +32,27 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+app.get('/ideas', (req, res) => {
+    Idea.find({}).lean()
+    .sort({date: 'desc'})
+    .then(ideas => {
+        res.render('ideas/index', {ideas: ideas});
+    });
+    
+});
+
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
+});
+
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    }).lean()
+    .then(idea => {
+        res.render('ideas/edit', {idea:idea});
+    });
+    
 });
 
 app.post('/ideas', (req, res) => {
@@ -55,11 +76,32 @@ app.post('/ideas', (req, res) => {
 
         });
     } else {
-        res.send('passed');
+        // res.send('passed');
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser).save()
+        .then(idea => {
+            res.redirect('/ideas');
+        })
     }
 });
 
-
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+        
+        idea.save()
+        .then(idea => {
+            res.redirect('/ideas');
+        });
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
---
+++ cmd
@@ -0,0 +1,5 @@
+mongo
+show dbs
+use vidjot-dev
+show collections
+db.ideas.find();
