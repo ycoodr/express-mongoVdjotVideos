602679245ca382817d153290242e626b9dc7b7a7
ycoodr <ycoodr@gmail.com>
2022-08-12 10:53:22 +0300

    passport login

--- config/passport.js
+++ config/passport.js
@@ -6,6 +6,31 @@ const User = mongoose.model('users');
 
 module.exports = function(passport){
     passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
-        console.log(email);
+        User.findOne({
+            email: email
+        }).then(user => {
+            if(!user){
+                return done(null, false, {message: 'no user found'});
+            }
+
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if(isMatch){
+                    return done(null, user);
+                } else {
+                    return done(null, false, {message: 'password incorrect'});
+                }
+            })
+        })
     }));
+
+    passport.serializeUser(function(user, done){
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done){
+        User.findById(id, function(err, user){
+            done(err, user);
+        });
+    });
 }
